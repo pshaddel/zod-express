576c8ba1317bf1674021bcbb7c9e836aad5e2a73
Poorshad <p.shaddel@gmail.com>
2023-03-30 16:50:07 +0200

    use swc for running tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "@swc/jest",
   },
 };
